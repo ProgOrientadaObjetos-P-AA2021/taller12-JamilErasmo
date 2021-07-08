4106940d9f9c13d40dccace2e70b473ebc25d74d
JamilErasmo <jamilerasmoq@gmail.com>
2021-07-08 17:27:58 -0500

    taller12

--- TallerJUnit/src/paquete1/Estudiante.java
+++ TallerJUnit/src/paquete1/Estudiante.java
@@ -1,7 +1,7 @@
-
 package paquete1;
 
 public class Estudiante {
+
     // 1.  Declarar
     // se declaran datos o atributos con visibilidad protegido
     // # nombresEstudiante: Cadena
@@ -12,6 +12,46 @@ public class Estudiante {
     protected String identificacionEstudiante;
     // # edadEstudiante: Entero
     protected int edadEstudiante;
-    
+
+    public void establecerNombresEstudiante(String nE) {
+        nombresEstudiante = nE;
+    }
+
+    public String obtenerNombresEstudiante() {
+        return nombresEstudiante;
+    }
+
+    public void establecerApellidoEstudiante(String aE) {
+        apellidosEstudiante = aE;
+    }
+
+    public String obtenerApellidoEstudiante() {
+        return apellidosEstudiante;
+    }
+
+    public void establecerIdentificacionEstudiante(String id) {
+        identificacionEstudiante = id;
+    }
+
+    public String obtenerIdentificacionEstudiante() {
+        return identificacionEstudiante;
+    }
+
+    public void establecerEdadEstudiante(int e) {
+        edadEstudiante = e;
+    }
+
+    public int obtenerEdadEstudiante() {
+        return edadEstudiante;
+    }
+
+    @Override
+    public String toString() {
+        String cadena = String.format("Nombre Estudiante: %s %s\nCédula: %s",
+                obtenerNombresEstudiante(),
+                obtenerApellidoEstudiante(),
+                obtenerIdentificacionEstudiante());
+        return cadena;
+    }
 
 }
--- TallerJUnit/src/paquete1/EstudianteDistancia.java
+++ TallerJUnit/src/paquete1/EstudianteDistancia.java
@@ -1,31 +1,80 @@
-
 package paquete1;
 
 import java.util.ArrayList;
 
-public class EstudianteDistancia extends Estudiante{
-       
+public class EstudianteDistancia extends Estudiante {
+
     private ArrayList<Nota> notas;
     private double mejorNota;
     private double peorNota;
     private double promedio;
-    
-    public void establecerNotas(ArrayList<Nota> ns){
+
+    public void establecerNotas(ArrayList<Nota> ns) {
         notas = ns;
-    
+
     }
-    
-    public void obtenerNotas(ArrayList<Nota> ns){
+
+    public ArrayList<Nota> obtenerNotas() {
+        return notas;
+
+    }
+
+    public void obtenerNotas(ArrayList<Nota> ns) {
         notas = ns;
-    
+
     }
-    
+
+    public void establecerMejorNota() {
+        double n = 0;
+        for (int i = 0; i < obtenerNotas().size(); i++) {
+            if (obtenerNotas().get(i).obtenerValor() > n) {
+                n = obtenerNotas().get(i).obtenerValor();
+            }
+        }
+        mejorNota = n;
+    }
+
+    public double obtenerMejorNota() {
+        return mejorNota;
+    }
+
+    public void establecerPeorNota() {
+        double n = 10;
+        for (int i = 0; i < obtenerNotas().size(); i++) {
+            if (obtenerNotas().get(i).obtenerValor() < n) {
+                n = obtenerNotas().get(i).obtenerValor();
+            }
+        }
+        peorNota = n;
+    }
+
+    public double obtenerPeorNota() {
+        return peorNota;
+    }
+
+    public void establecerPromedio() {
+        double suma = 0;
+        for (int i = 0; i < notas.size(); i++) {
+            suma = suma + notas.get(i).obtenerValor();
+        }
+        promedio = suma / notas.size();
+    }
+
+    public double obtenerPromedio() {
+        return promedio;
+    }
+
     @Override
-    public String toString(){
-        return "";
+    public String toString() {
+        String cadena = String.format("%sNotas: \n", super.toString());
+        for (int i = 0; i < obtenerNotas().size(); i++) {
+            cadena = String.format("%s%.1f", cadena, obtenerNotas().get(i).
+                    obtenerValor());
+        }
+        cadena = String.format("%sProemdio: %.1fMejor Nota: %.1f"
+                + "Peor Nota: %.1f", cadena,
+                obtenerPromedio(), obtenerMejorNota(), obtenerPeorNota());
+        return cadena;
     }
-    
-    
-    
 
 }
